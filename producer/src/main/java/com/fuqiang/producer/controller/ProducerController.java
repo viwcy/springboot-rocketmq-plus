5f1f64f3035cbef536dfa8b2ff8837119c471d38
fuqiang0406 <3386216884@qq.com>
2020-12-31 11:21:49 +0800

    update

--- producer/src/main/java/com/fuqiang/producer/controller/ProducerController.java
+++ producer/src/main/java/com/fuqiang/producer/controller/ProducerController.java
@@ -48,7 +48,7 @@ public class ProducerController {
 //        SendResult sendResult3 = rocketMQTemplate.syncSendOrderly("orderly-topic", "订单完成", orderId);
 //        log.info("message：{}，orderId：{}，queueId：{}", "订单完成", orderId, sendResult3.getMessageQueue().getQueueId());
         for (int i = 1; i < 10; i++) {
-            SendResult sendResult = rocketMQTemplate.syncSendOrderly("orderly-topic", "测试顺序消息" + i, String.valueOf(i));
+            SendResult sendResult = rocketMQTemplate.syncSendOrderly("orderly_topic", "测试顺序消息" + i, String.valueOf(i));
             log.info("message：{}，id：{}，queueId：{}", "测试顺序消息" + i, i, sendResult.getMessageQueue().getQueueId());
         }
         return "OK";
@@ -57,7 +57,7 @@ public class ProducerController {
     @PostMapping("/randomsend")
     public String randomsend(@RequestParam("id") String id) {
         for (int i = 1; i < 10; i++) {
-            SendResult sendResult = rocketMQTemplate.syncSend("random-topic", "随机消息" + i);
+            SendResult sendResult = rocketMQTemplate.syncSend("random_topic", "随机消息" + i);
             log.info("message：{}，id：{}，queueId：{}", "随机消息" + i, i, sendResult.getMessageQueue().getQueueId());
         }
         return "OK";
